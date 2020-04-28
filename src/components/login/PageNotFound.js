59e1a61abfbd435072ef23297160169af4e1af6b
juniorMe <gatecheck28@gmail.com>
2020-04-28 19:43:30 +0530

    updated fallback

--- src/components/login/PageNotFound.js
+++ src/components/login/PageNotFound.js
@@ -1,13 +1,13 @@
-import {images} from '../../image';
+// import {images} from '../../image';
 import React from 'react'
 import { Paper } from "@material-ui/core";
 import '../.././App.css';
 
 export default function PageNotFound() {
     return (
-        <Paper>
-            <img src={images.notFoundPage} className = "pageNotFound" />
-            {/* <h4>Page Not Found...</h4> */}
+        <Paper style={{alignContent: "center", margin: "20px"}}>
+            {/* <img src={images.notFoundPage} className = "pageNotFound" /> */}
+            <h4>Page Not Found...</h4>
         </Paper>
     )
 }
