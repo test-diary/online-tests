d456df38d66683580c093a97c040079ef7aeb66b
juniorMe <gatecheck28@gmail.com>
2020-05-02 18:20:40 +0530

    added result table

--- src/components/login/PageNotFound.js
+++ src/components/login/PageNotFound.js
@@ -1,4 +1,4 @@
-// import {images} from '../../image';
+import {images} from '../../image';
 import React from 'react'
 import { Paper } from "@material-ui/core";
 import '../.././App.css';
@@ -6,8 +6,7 @@ import '../.././App.css';
 export default function PageNotFound() {
     return (
         <Paper style={{alignContent: "center", margin: "20px"}}>
-            {/* <img src={images.notFoundPage} className = "pageNotFound" /> */}
-            <h4>Page Not Found...</h4>
+            <img src={images.notFoundPage} alt="abc" className = "pageNotFound" />
         </Paper>
     )
 }
--- src/components/login/Pageone.js
+++ src/components/login/Pageone.js
@@ -1,12 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { TableContainer, Table, TableBody, TableHead, TableCell, TableRow, Paper } from "@material-ui/core";
 
-export default class Pageone extends Component {
-    render() {
+const createBody = (rows) => {
+    return rows.map((row) => {
         return (
-            <div>
-                <h1 style ={{backgroundColor: "pink"}}>Hello friend....</h1>
-                <h3 style ={{backgroundColor: "gray"}}>Page 1....</h3>
-            </div>
+            <TableRow>
+                {
+                    row.map((cell) => <TableCell align="center">{cell}</TableCell>)
+                }
+            </TableRow>
         )
-    }
+    })
 }
+
+const createHeader = (headers) => {
+    return (
+        <TableRow>
+            {
+                headers.map((head) => <TableCell align="center" style={{fontWeight: "bold", color: "white", backgroundColor: "gray"}}>{head}</TableCell>)
+            }
+        </TableRow>
+    )
+}
+const ResultAnalysis = (props) => {
+    const headers = [
+        "Correct Answers",
+        "Wrong Answers",
+        "Not Attempted"
+    ]
+    const data = [
+        [5, 3, 2],
+        [5, 3, 2],
+        [5, 3, 2],
+        [5, 3, 2],
+        [5, 3, 2]
+    ]
+    return (
+        <TableContainer style={{margin: "30px 100px 0 250px", width: "350px"}} component={Paper}>
+            <Table size="small" aria-label="customized table" stickyHeader>
+                <TableHead style={{backgroundColor: "green"}}>
+                    {
+                        createHeader(headers)
+                    }
+                </TableHead>
+                <TableBody>
+                    {createBody(data)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default ResultAnalysis;
