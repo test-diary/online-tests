09feff04fbdfa64c83dc129c79bfeeec8e77d411
Shubhamsoniengg <shubham.soni@successive.tech>
2020-05-05 17:42:50 +0530

    Add question component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './compoents/login/Login'
+import Question from './compoents/onlineQuestions/Question'
 
 function App() {
   return (
     <div className="App">   
       <Login />
+      <Question/>
     </div>
   );
 }
